3d7cdee2fd32087f8ea674d046e1574b2edb0ed2
petromamboubee <gm.boubee@gmail.com>
2022-08-17 19:06:13 -0300

    finalizei a questão 7

--- src/objPlayground.js
+++ src/objPlayground.js
@@ -34,8 +34,20 @@
   arrayGenerator('entries', { sum: 3, mult: 2, div: 0, sub: -1 }) // [ [ 'sum', 3 ], [ 'mult', 2 ], [ 'div', 0 ], [ 'sub', -1 ] ]
 */
 
-const calculator = (number1, number2) => {};
+const calculator = (number1, number2) => {
+  let sum = number1 + number2;
+  let mult = number1 * number2;
+  let div = Math.floor(number1 / number2);
+  let sub = number1 - number2;
+  return { sum, mult, div, sub };
+};
 
-const arrayGenerator = (type, object) => {};
+const arrayGenerator = (type, object) => {
+  if (type === 'keys') {
+   return Object.keys(object);
+} if (type === 'values') {
+  return Object.values(object);
+} return Object.entries(object);
+};
 
 module.exports = { calculator, arrayGenerator };
