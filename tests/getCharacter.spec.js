b0f6ea0eea3d1c34f0cd1f6f726b087c3c9415c8
petromamboubee <gm.boubee@gmail.com>
2022-08-18 17:04:00 -0300

    terminei a 9

--- tests/getCharacter.spec.js
+++ tests/getCharacter.spec.js
@@ -43,13 +43,18 @@ Retorno:
 
 describe('9 - Implemente os casos de teste da função `getCharacter`', () => {
   it('Verifica se a função `getCharacter` retorna o objeto do personagem corretamente.', () => {
-    fail('Teste vazio!');
     // ESCREVA SEUS TESTES ABAIXO:
+    expect(getCharacter()).toBe(undefined)
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    expect(getCharacter('Arya').name).toMatch(/Arya Stark/)
     // Teste se a função retorna o objeto correto para o parâmetro 'Arya',
+    expect(getCharacter('Brienne').name).toMatch(/Brienne Tarth/)
     // Teste se a função retorna o objeto correto para o parâmetro 'Brienne',
+    expect(getCharacter('Melissandre').name).toMatch(/Melissandre/)
     // Teste se a função retorna o objeto correto para o parâmetro 'Melissandre',
+    expect(getCharacter('melissandre').name).toMatch(/Melissandre/)
     // Teste se o parâmetro não é Case Sensitive, ou seja, independente de conter letras maiúsculas ou minúsculas retorna o mesmo objeto relativo a ele.
+    expect(getCharacter('boubee')).toBe(undefined)
     // Teste se ao passar um nome que não está na tabela, a função retorna undefined.
   });
 });
